19092638c46fcd8c1810f2382bcc847e826f30af
Jonathan Hildenbrand <jonathanhild@users.noreply.github.com>
2020-09-02 09:14:31 -0700

    add winco_foods map dataset & script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # geoguessrmaps
-Python scripts to produce GeoGuessr maps
+Python scripts to produce latitude, longitude coordinates for uploading to GeoGuessr's Map Maker.
+
+![](./img/geoguessr.png)
+
+## Setup API Keys
+Place your Google Maps API key in `./.env`:
+
+```
+GOOGLE_API_KEY={secret_key}
+```
+
+## Maps
+### WinCo Foods
+Play: https://www.geoguessr.com/maps/5f4fbb99ef52fd00018796f6
+
+WinCo Foods is an employee-owned grocery store chain in the Western US. The WinCo name comes from a 1999 company contest to rename the chain, which the winning name is a portmanteau of Winning Company and the states in which the chain operates in (Washington, Idaho, Nevada, California, and Oregon). But don't assume that they haven't expanded since then!
+
+## License
+This is free and unencumbered software released into the public domain.
---
+++ winco_foods/winco_foods.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python3
+
+import os
+import json
+import csv
+
+from geopy.geocoders import GoogleV3
+from geopy.extra.rate_limiter import RateLimiter
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv('GOOGLE_API_KEY')
+
+os.chdir('./winco_foods')
+
+store_latlon = []
+errors = []
+
+with open('store_data.json', 'r') as json_data:
+    store_data = json.load(json_data)
+
+geolocator = GoogleV3(user_agent='wincoGeocoder', api_key=api_key)
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+for store in store_data['stores']:
+    address = store['address']
+    city = store['city']
+    state = store['state']
+    zip_code = store['zip']
+    geo_addr = f'{address}, {city}, {state} {zip_code}'
+    location = geocode(geo_addr)
+    if location is not None:
+        store_latlon.append((location.latitude, location.longitude))
+        print(
+            f'Found coordinates for: {geo_addr}, at latitude: {location.latitude} and longitude: {location.longitude}')
+    else:
+        print(
+            f'Did not found coordinates for: {geo_addr}')
+        errors.append(geo_addr + '\n')
+
+with open('winco_locations.csv', 'w') as csv_file:
+    location_file = csv.writer(csv_file)
+    for store in store_latlon:
+        csv_row = store
+        location_file.writerow(csv_row)
+    csv_file.close()
+
+with open('error.txt', 'w') as txt_file:
+    txt_file.writelines(errors)
+    txt_file.close()
